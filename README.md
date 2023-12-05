cc852aa856c6ca1dc937b4cc419e34318cae4a4f
Daniel Jutila <daniel.jutila@gmail.com>
2023-12-04 18:03:57 -0600

    Code works well! Added all functions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # daily-planner
+
+
+Attribute questions
+https://stackoverflow.com/questions/12510593/how-to-get-the-attribute-values-of-the-table-row-using-jquery
+
+
+Getting parent
+https://stackoverflow.com/questions/9922032/jquery-get-the-parent-element-id-of-a-clicked-element 
+
+
+AMPM
+https://stackoverflow.com/questions/17003202/how-to-get-am-or-pm 
--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,41 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(document).ready(function () {
+  var now = dayjs();
+
+  var currentHour = dayjs().hour();
+  for (var hour = 9; hour <= 17; hour++) {
+    //made to show proper AM and PM
+    var displayHour = (hour % 12 === 0) ? 12 : hour % 12;
+    var ampm = (hour < 12) ? 'AM' : 'PM';
+
+    //makes the time blocks
+    var timeBlock = $('<div>').attr('id', 'hour-' + hour).addClass('row time-block');
+    var numberBlock = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(displayHour + ampm);
+    var noteInputArea = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', '3');
+    var saveButton = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save').html('<i class="fas fa-save" aria-hidden="true"></i>');
+    
+    //puts correct color for backgrounds.
+    if (hour < currentHour) {
+      timeBlock.addClass('past');
+    } else if (currentHour === hour) {
+      timeBlock.addClass('present');
+    } else {
+      timeBlock.addClass('future');
+    }
+    timeBlock.append(numberBlock, noteInputArea, saveButton);
+    $('.container-lg').append(timeBlock);
+    $('#currentDay').text('Current day ' + now.format('MMMM D, YYYY'));
+    //puts description into each block
+    var putDescription = JSON.parse(localStorage.getItem(timeBlock.attr('id')));
+    if (putDescription !== null) {
+      noteInputArea.text(putDescription);
+    }
+  }
+  //save button function
+  $('.saveBtn').children().on('click', function (event) {
+    event.preventDefault();
+    //gets the text that was written next to the save button.
+    var input = $(this).parent().parent().find('.description').val();
+    var whichBlock = $(this).parent().parent().attr('id');
+    localStorage.setItem(whichBlock, JSON.stringify(input));
+  })
 });
